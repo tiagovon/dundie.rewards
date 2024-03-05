30fa1dccd2edab20668b759ca93139e665548722
tiagovon <tiago3242@gmail.com>
2024-03-05 18:07:04 -0300

    closes #8

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="dundie",
+    version ="0.1.0",
+    description="Reward Point System for Dunder Mifflin",
+    author="Bruno Rocha",
+    packges=["dundie"]
+)
