05e443b53950df07fbbff6083010a12de7e2a194
tiagovon <tiago3242@gmail.com>
2024-03-13 14:06:17 -0300

    test git-hub

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,11 +1,18 @@
 from dundie.core import load
 from tests.constants import PEOPLE_FILE
 import pytest
+import uuid
+import os
+
+@pytest.mark.unit
+@pytest.mark.high
+def test_load_positive_had_2_people(request):
+    """test load function"""
+    assert len(load(PEOPLE_FILE)) == 2
 
 
 @pytest.mark.unit
 @pytest.mark.high
-def test_load():
+def test_load_positive_dirst_name_starts_with_t(request):
     """test load function"""
-    assert len(load(PEOPLE_FILE)) == 2
-    #assert load('tests/assets/people.csv')[0][0] =="t"
+    assert load(PEOPLE_FILE)[0][0] =="t"
