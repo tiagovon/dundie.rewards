6126e3ea204471e34b4b47c5eae176f64cc5bd5a
tiagovon <tiago3242@gmail.com>
2024-03-27 11:37:34 -0300

    aulao

--- dundie/cli.py
+++ dundie/cli.py
@@ -2,7 +2,7 @@ import pkg_resources
 import rich_click as click
 from rich.console import Console
 from rich.table import Table
-
+import json
 from dundie import core
 
 click.rich_click.USE_RICH_MARKUP = True
@@ -41,3 +41,47 @@ def load(filepath):
 
     console = Console()
     console.print(table)
+@main.command()
+@click.option("--dept", required=False)
+@click.option("--email", required=False)
+@click.option("--output", default=None)
+def show(output, **query):
+    """Shows information about user or dept."""
+    result = core.read(**query)
+    if output:
+        with open(output, "w") as output_file:
+            output_file.write(json.dumps(result))
+
+    if not result:
+        print("Nothing to show")
+
+    table = Table(title="Dunder Mifflin Report")
+    for key in result[0]:
+        table.add_column(key.title(), style="magenta")
+
+    for person in result:
+        table.add_row(*[str(value) for value in person.values()])
+
+    console = Console()
+    console.print(table)
+
+@main.command()
+@click.argument("value", type=click.INT, required=True)
+@click.option("--dept", required=False)
+@click.option("--email", required=False)
+@click.pass_context
+def add(ctx, value, **query):
+    """Add points to the user or dept."""
+    core.add(value, **query)
+    ctx.invoke(show, **query)
+
+
+@main.command()
+@click.argument("value", type=click.INT, required=True)
+@click.option("--dept", required=False)
+@click.option("--email", required=False)
+@click.pass_context
+def remove(ctx, value, **query):
+    """Removes points from the user or dept."""
+    core.add(-value, **query)
+    ctx.invoke(show, **query)
